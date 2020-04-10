5c11827dfead69fdbdcbdbaa62f7b1b0290fff18
Andrei Abramchuk <stilegs@gmail.com>
2020-04-10 10:52:01 +0300

    feat: add webpack

---
+++ src/script.js
@@ -0,0 +1,6 @@
+const main = document.querySelector('.main');
+
+
+main.addEventListener('click', () => {
+  main.style.transform = 'scale(1.2)';
+});
---
+++ webpack.config.js
@@ -0,0 +1,56 @@
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = (env, options) => {
+  const isProd = options.mode === 'production';
+
+  const config = {
+    mode: isProd ? 'production' : 'development',
+    devtool: isProd ? 'none' : 'source-map',
+    watch: !isProd,
+    entry: ['./src/script.js', './src/sass/style.scss'],
+    output: {
+      filename: 'script.js',
+      path: path.join(__dirname, 'dist'),
+    },
+    module: {
+      rules: [
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+            },
+          },
+        },
+        {
+          test: /\.scss$/,
+          use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+        },
+        {
+          test: /\.(png|svg|jpe?g|gif)$/,
+          use: [
+            {
+              loader: 'file-loader',
+            },
+          ],
+        },
+        {
+          test: /\.html$/i,
+          loader: 'html-loader',
+        },
+      ],
+    },
+    plugins: [
+      new CleanWebpackPlugin(),
+      new HtmlWebpackPlugin({ template: 'index.html' }),
+      new MiniCssExtractPlugin({ filename: 'style.css' }),
+    ],
+  };
+
+  return config;
+};
