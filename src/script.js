4964ee35f393315a27f8d1fee4f2c596261fdea7
Andrei Abramchuk <stilegs@gmail.com>
2020-04-12 10:34:49 +0300

    feat: add categories

--- src/script.js
+++ src/script.js
@@ -3,6 +3,18 @@ import cards from './cards';
 const cardItems = document.querySelector('.card-items');
 
 
+function createCategory(title, img, i) {
+  console.log(title, img);
+  const cardEl = document.createElement('div');
+  cardEl.classList.add('cat-card');
+  cardEl.setAttribute('data-index', i + 1);
+  cardEl.innerHTML = `
+    <img src="${img}">
+    <div class="cat-card__title">${title}</div>
+  `;
+  cardItems.append(cardEl);
+}
+
 function createCard(word) {
   console.log(word);
   const cardEl = document.createElement('div');
@@ -18,6 +30,22 @@ function createCard(word) {
   cardItems.append(cardEl);
 }
 
-cards[1].forEach((card, i) => {
-  createCard(cards[1][i]);
+
+function renderWordsSet(setIndex) {
+  cardItems.innerHTML = '';
+  cards[setIndex].forEach((card, i) => {
+    createCard(cards[setIndex][i]);
+  });
+}
+
+cards[0].forEach((card, i) => {
+  createCategory(cards[0][i], cards[i + 1][0].image, i);
+});
+
+
+cardItems.addEventListener('click', (e) => {
+  if (e.target !== cardItems) {
+    const index = e.target.getAttribute('data-index');
+    renderWordsSet(index);
+  }
 });
