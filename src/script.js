d7e37a99bb7eaa32afb01635fc4cfb74d237704c
Andrei Abramchuk <stilegs@gmail.com>
2020-04-13 12:28:16 +0300

    feat: add rotate button

--- src/script.js
+++ src/script.js
@@ -3,26 +3,29 @@ import cards from './cards';
 // TODO:
 // fix header (<1200px)
 
-const container = document.querySelector('.container');
+const container = document.querySelector('.cards-container');
 const burger = document.querySelector('#menu');
-function createCategory(cat, title, img, i) {
+const title = document.querySelector('.title');
+
+function createCategory(cat, name, img, i) {
   const cardEl = document.createElement('div');
   cardEl.classList.add('cat-card');
   cardEl.setAttribute('data-index', i + 1);
   cardEl.innerHTML = `
     <img src="${img}" data-index="${i + 1}">
-    <div class="cat-card__title" data-index="${i + 1}">${title}</div>
+    <div class="cat-card__title" data-index="${i + 1}">${name}</div>
   `;
   cat.append(cardEl);
 }
 
 function createCard(parent, word) {
-  const cardEl = document.createElement('div');
+  const card = document.createElement('div');
   const front = document.createElement('div');
   const back = document.createElement('div');
   const enTitle = document.createElement('div');
   const ruTitle = document.createElement('div');
-  cardEl.classList.add('card');
+  const rotate = document.createElement('div');
+  card.classList.add('card');
   front.classList.add('front');
   back.classList.add('back');
   enTitle.classList.add('card-title');
@@ -31,14 +34,31 @@ function createCard(parent, word) {
   ruTitle.textContent = word.translation;
   front.style.backgroundImage = `url(${word.image})`;
   back.style.cssText = `background-image: url(${word.image});`;
-  cardEl.append(front);
-  cardEl.append(back);
+  rotate.classList.add('rotate');
+  rotate.setAttribute('id', 'rotate');
+  rotate.style.backgroundImage = 'url("img/rotate.svg")';
+  card.append(front);
+  card.append(back);
   front.append(enTitle);
+  front.append(rotate);
   back.append(ruTitle);
-  parent.append(cardEl);
+  parent.append(card);
+
+  card.addEventListener('click', (e) => {
+    if (!e.target.innerHTML) {
+      e.target.offsetParent.style.cssText = `background-image: url(${word.image}); transform: rotateY(180deg)`;
+      e.target.offsetParent.nextElementSibling.style.cssText = `background-image: url(${word.image}); transform: rotateY(360deg); filter: saturate(0%);`;
+    }
+  });
+
+  card.addEventListener('mouseleave', (e) => {
+    e.target.firstElementChild.style.cssText = `background-image: url(${word.image}); transform: rotateY(0deg)`;
+    e.target.lastElementChild.style.cssText = `background-image: url(${word.image}); transform: rotateY(180deg)`;
+  });
 }
 
 function renderWordsSet(setIndex) {
+  title.textContent = cards[0][setIndex - 1];
   container.innerHTML = '';
   const cardItems = document.createElement('div');
   cardItems.classList.add('card-items');
@@ -49,6 +69,7 @@ function renderWordsSet(setIndex) {
 }
 
 function renderCategories() {
+  title.textContent = 'Categories';
   container.innerHTML = '';
   const categoriesEl = document.createElement('div');
   cards[0].forEach((card, i) => {
