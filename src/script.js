fd12f15d5acbbcc3029dda1646f8a0731399a396
Andrei Abramchuk <stilegs@gmail.com>
2020-04-19 12:40:52 +0300

    refactor: optimize images

--- src/script.js
+++ src/script.js
@@ -1,9 +1,7 @@
 import renderCategories from './renderCategories';
 import burgerHandler from './burgerHandler';
 import gameMode from './gameMode';
-// import renderStatistics from './renderStatistics';
 
 gameMode();
 burgerHandler();
 renderCategories();
-// renderStatistics();
